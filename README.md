b100c24625565de8a81c6bf9bdd7a8c9021e849d
Weston Platter <westonplatter@gmail.com>
2018-08-10 14:33:13 -0600

    it works.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 RPOPs - restful panda operations
 
 
+# development
+Run this,
+```
+FLASK_ENV=development flask run
+```
+
+
+# production
+Run this,
+```
+
+```
--- app.py
+++ app.py
@@ -3,23 +3,28 @@ import pandas as pd
 import json
 app = Flask(__name__)
 
+
 @app.route('/rolling_mean_series', methods=['POST'])
 def rolling_mean_series():
     j = request.json
     data = j["data"]
 
-    if "window" in j:
-        window = j["window"]
-    else:
-        window = 6
+    window = (j["window"] if "window" in j else 6)
 
     # @TODO redo this so it's not serializing and deserializing
     mean = pd.Series(data).rolling(window=window).mean()
     json_values = mean.to_json(orient="values")
     array = json.loads(json_values)
 
-    result = {
-        "result": array,
-        "errors": None }
-
+    result = {"result": array, "errors": None}
     return jsonify(result)
+
+
+@app.route("/")
+def hello():
+    """docstring"""
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
